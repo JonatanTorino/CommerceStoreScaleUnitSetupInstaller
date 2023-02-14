637819b03737aa5a97c56523db0a4dd0943f7588
Jonatan Torino <jonatan.torino@hotmail.com>
2023-02-14 13:13:09 +0000

    Mejora en el readme y en los comentarios del ConfigDependence script

--- CheckD365foConfigDependency.ps1
+++ CheckD365foConfigDependency.ps1
@@ -14,22 +14,28 @@ $url = "https://AOSServices"
 $miSysAADClientTable = $url+"/?mi=SysAADClientTable&lng=en-us"
 
 Write-Host 
-Write-Host -ForegroundColor yellow "Revisar que esten todos los ClientId cargados"
+Write-Host -ForegroundColor yellow "Revisar que esten cargados todos los ClientId"
 Write-Host $miSysAADClientTable
+Write-Host "Configuración propuesta, para que todos los entornos manejen los mismos nombres (util "
 Write-Host "UserID: RetailServiceAccount | Name: Cmm-POS"
 Write-Host "UserID: RetailServiceAccount | Name: Cmm-RS"
 Write-Host "UserID: RetailServiceAccount | Name: Cmm-Async"
 
 $miRetailSharedParameters = $url+"/?mi=RetailSharedParameters&lng=en-us"
 Write-Host 
-Write-Host -ForegroundColor yellow "Revisar que esten cargados:"
-Write-Host $miRetailSharedParameters
-Write-Host "IDENTITY PROVIDERS  | con Type = Azure Active Directory"
-Write-Host "RELYING PARTIES     | con Type = Public y UserType = Worker"
-Write-Host "SERVER RESOURCE IDS | con ServerResourceIds comenzando 'api://'"
-Write-Host 
+Write-Host -ForegroundColor yellow "Revisar que esten cargados las configuraciones en CommerceSharedParameters > Identity Providers"
+Write-Host "Este pantalla cuenta con 3 grillas, cada grilla de inferior está invuclada con el registro de la grilla superior"
 
-$miRetailSharedParameters = $url+"/?mi=RetailSharedParameters&lng=en-us"
+Write-Host $miRetailSharedParameters
+Write-Host "Grilla IDENTITY PROVIDERS: crear un solo registro (si no existe) con el TenantId que está EN EL ARCHIVO ChannelConfig  "
+Write-Host "Issuer: https://sts.windows.net/TENANT-ID | Name: Cmm-CSU | Type: Azure Active Directory"
+Write-Host 
+Write-Host "Grilla RELYING PARTIES: crear un solo registro, vinculado al TenantId creado anteriormente"
+Write-Host "ClientId: el que corresponde al POS | Type: Public | UserType: Worker | Name: CSU-POS"
+Write-Host 
+Write-Host "Grilla SERVER RESOURCE IDS: crear un solo registro, vinculado al RELYING PARTIES creado anteriormente"
+Write-Host "ServerResourceIds: 'api://DEL RETAILSERVER' | Name: CSU-RS"
+Write-Host 
 
 # Write-Host "Presione una tecla para continuar"
 # [Console]::ReadKey()
--- readme.md
+++ readme.md
@@ -1,8 +1,16 @@
-Esta carpeta cuenta con varios scripts para la instalación inicial de un CommerceScaleUnit estándar de Microsoft.
+Esta carpeta cuenta con varios scripts para la instalación inicial de un CommerceScaleUnit (SEALED) estándar de Microsoft.
+
+1) Para la instalación, es necesario tomar el archivo __SAMPLE_Env__.json, y hacer una copia con nombre igual que el entorno.
+2) completar los campos del archivo de configuración
+3) ejecutar el script InstallScaleUnit.ps1 pasando el fullpath del archivo de configuración creado.
+
+El resto de los archivos Scripts no es necesario modificarlos, al menos que encuentren un error o consideren agregar una mejora.
 
 **InstallScaleUnit.ps1**
-Es el archivo inicial donde se deben proveer los Azure AppId y Thumbprint de certificados para la instalación.
-Este script invoca a los demás scripts en el orden necesario para la instalación.
+Este script invoca a los demás scripts en el orden necesario para la instalación, invocando a scripts que realizan chequeos de dependencias para la instalación, así como a otros posteriores que ajustan valores que vienen por defecto en la instalación.
+
+**CheckD365foConfigDependency.ps1**
+Advierte que se requiere agregar de forma manual en la aplicación de D365FO, la configuración de los AppId que se van a usar para esta instalación.
 
 **ChangeAsyncInterval.ps1**
 Sirve para cambiar los tiempos de sincronización del AsyncClient.
