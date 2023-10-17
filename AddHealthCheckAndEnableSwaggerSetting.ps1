fd9ecd1f12a48ce3948502275b22c13db250fe5b
Jonatan Torino <Jonatan.Torino@hotmail.com>
2023-10-17 22:18:35 +0000

    Agrego una validacion en el archivo AddHealthCheckAndEnableSwaggerSetting.ps1

--- AddHealthCheckAndEnableSwaggerSetting.ps1
+++ AddHealthCheckAndEnableSwaggerSetting.ps1
@@ -4,18 +4,35 @@
 $rutaDelArchivo = "C:\Program Files\Microsoft Dynamics 365\10.0\Commerce Scale Unit\Microsoft\RetailServer\bin\Microsoft.Dynamics.Retail.RetailServer.AspNetCore.dll.config"
 
 [xml]$xml = Get-Content $rutaDelArchivo
+# Obtener el nodo padre existente
 $nodoPadre = $xml.SelectSingleNode("/configuration/appSettings")
 
-#HealthCheck.Extensions.ShowAssemblyFiles
-$nuevoNodo = $xml.CreateElement("add")
-$nuevoNodo.SetAttribute("key", "HealthCheck.Extensions.ShowAssemblyFiles")
-$nuevoNodo.SetAttribute("value", "true")
-$nodoPadre.PrependChild($nuevoNodo)
+# Verificar si un nodo con el mismo nombre y atributos ya existe
+$guardarXml = $false
+foreach ($nodoExistente in $nodoPadre.ChildNodes) {
 
-#EnableSwagger
-$nuevoNodo = $xml.CreateElement("add")
-$nuevoNodo.SetAttribute("key", "EnableSwagger")
-$nuevoNodo.SetAttribute("value", "true")
-$nodoPadre.PrependChild($nuevoNodo)
+    #HealthCheck.Extensions.ShowAssemblyFiles
+    if ($nodoExistente.Name -eq "add" -and $nodoExistente.GetAttribute("key") -eq "HealthCheck.Extensions.ShowAssemblyFiles") {
+        $nuevoNodo = $xml.CreateElement("add")
+        $nuevoNodo.SetAttribute("key", "HealthCheck.Extensions.ShowAssemblyFiles")
+        $nuevoNodo.SetAttribute("value", "true")
+        $nodoPadre.PrependChild($nuevoNodo)
 
-$xml.Save($rutaDelArchivo)
+        $guardarXml = $true
+        break
+    }
+
+    #EnableSwagger
+    if ($nodoExistente.Name -eq "add" -and $nodoExistente.GetAttribute("key") -eq "HealthCheck.Extensions.ShowAssemblyFiles") {
+        $nuevoNodo = $xml.CreateElement("add")
+        $nuevoNodo.SetAttribute("key", "EnableSwagger")
+        $nuevoNodo.SetAttribute("value", "true")
+        $nodoPadre.PrependChild($nuevoNodo)
+        $guardarXml = $true
+        break
+    }
+}
+
+if ($guardarXml) {
+    $xml.Save($rutaDelArchivo)
+}
