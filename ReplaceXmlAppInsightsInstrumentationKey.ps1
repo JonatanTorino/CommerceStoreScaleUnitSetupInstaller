8374233b006e755d91d953dc1e71682b7d95f809
Jonatan Torino <Jonatan.Torino@hotmail.com>
2023-10-25 15:42:21 +0000

    Agrego un script para reemplazar configuraciones del XML de configuracion del canal

---
+++ ReplaceXmlAppInsightsInstrumentationKey.ps1
@@ -0,0 +1,39 @@
+[CmdletBinding()]
+param (
+    [string]
+    [ValidateNotNullOrEmpty()]$jsonFile
+)
+
+Write-Host 
+Write-Host "==============================="
+Write-Host "        Replace Xml Config AppInsightsInstrumentationKey        "
+Write-Host "==============================="
+Write-Host 
+
+#Parseo el archivo json para leer sus propiedades
+$json = Get-Content $jsonFile -Raw | ConvertFrom-Json
+
+#Inicializo cada variable del json
+[string]$AppInsightsInstrumentationKey=$json.AppInsightsInstrumentationKey
+[string]$ChannelConfig = $json.ChannelConfig
+[string]$EnvironmentId = $json.EnvironmentId
+
+# Cargar el archivo XML
+[xml]$xml = Get-Content $ChannelConfig
+
+# Seleccionar todos los nodos 'add' con atributo 'key' que termine en 'AppInsightsInstrumentationKey'
+$nodos = $xml.SelectNodes('//add[contains(@key, "AppInsightsInstrumentationKey")]')
+
+# Recorrer y editar los nodos encontrados
+foreach ($nodo in $nodos) {
+    # Modificar el atributo 'value'
+    $nodo.SetAttribute("value", $AppInsightsInstrumentationKey)  # Reemplaza "NuevoValor" con el valor deseado
+}
+
+$nodos = $xml.SelectNodes('//add[contains(@key, "EnvironmentId")]')
+$nodo.SetAttribute("value", $EnvironmentId)  # Reemplaza "NuevoValor" con el valor deseado
+
+# Guardar los cambios en el archivo
+$xml.Save($ChannelConfig)
+
+Write-Host "Los atributos 'value' se han editado en el archivo XML."
