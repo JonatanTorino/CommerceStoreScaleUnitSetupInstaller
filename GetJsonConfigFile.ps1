2378cb610261543c9e038c851a4c4504b3cb783e
Jonatan Torino <jonatan.torino@hotmail.com>
2023-11-03 23:10:42 +0000

    .

--- GetJsonConfigFile.ps1
+++ GetJsonConfigFile.ps1
@@ -4,23 +4,23 @@ param (
 
 # Comprobar si el parámetro no fue pasado
 if ($null -eq $jsonFile -or -not (Test-Path -Path $jsonFile)) {
-        $nombreEntorno = [System.Environment]::MachineName
-        $jsonFileDefault = Get-ChildItem -Path '.\ConfigFiles\' -Filter "$nombreEntorno.json" | Select-Object -ExpandProperty FullName
-        Write-Host -ForegroundColor Yellow "Script invacado sin especificar ruta de archivo de configuracion JSON."
-        Write-Host -ForegroundColor Yellow "Por lo tanto se busca por defecto el siguiente archivo:"
-        Write-Host  "   .\ConfigFiles\$nombreEntorno.json"
+    $nombreEntorno = [System.Environment]::MachineName
+    $jsonFileDefault = Get-ChildItem -Path '.\ConfigFiles\' -Filter "$nombreEntorno.json" | Select-Object -ExpandProperty FullName
+    Write-Host -ForegroundColor Yellow "Script invacado sin especificar ruta de archivo de configuracion JSON."
+    Write-Host -ForegroundColor Yellow "Por lo tanto se busca por defecto el siguiente archivo:"
+    Write-Host  "   .\ConfigFiles\$nombreEntorno.json"
 
-        # if ((Test-Path $jsonFile -PathType Leaf) -eq $false) {
-        if (($null -ne $jsonFileDefault) -and
+    # if ((Test-Path $jsonFile -PathType Leaf) -eq $false) {
+    if (($null -ne $jsonFileDefault) -and
         (Test-Path $jsonFileDefault -PathType Any)) {
-            Write-Host "ARCHIVO ENCONTRADO"
-        }
-        else {
-            Write-Host -ForegroundColor Red "Archivo no encontrado"
-            throw [System.IO.FileNotFoundException] "$jsonFileDefault not found."
-        }
-    
-        $jsonFile = $jsonFileDefault
+        Write-Host "ARCHIVO ENCONTRADO"
     }
+    else {
+        Write-Host -ForegroundColor Red "Archivo no encontrado"
+        throw [System.IO.FileNotFoundException] "$jsonFileDefault not found."
+    }
+    
+    $jsonFile = $jsonFileDefault
+}
 
-    Write-Output $jsonFile
+Write-Output $jsonFile
