bb62525a42e3aeb67de6a418008972d4df4adeed
Jonatan Torino <jonatan.torino@hotmail.com>
2024-03-11 22:39:32 +0000

    Fix en  AddHealthCheck

--- AddHealthCheckAndEnableSwaggerSetting.ps1
+++ AddHealthCheckAndEnableSwaggerSetting.ps1
@@ -9,28 +9,44 @@ $nodoPadre = $xml.SelectSingleNode("/configuration/appSettings")
 
 # Verificar si un nodo con el mismo nombre y atributos ya existe
 $guardarXml = $false
+$nodeNotFoundHealthCheck = $true
+$nodeNotFoundEnableSwagger = $true
 foreach ($nodoExistente in $nodoPadre.ChildNodes) {
 
     #HealthCheck.Extensions.ShowAssemblyFiles
-    if ($nodoExistente.Name -eq "add" -and $nodoExistente.GetAttribute("key") -eq "HealthCheck.Extensions.ShowAssemblyFiles") {
-        $nuevoNodo = $xml.CreateElement("add")
-        $nuevoNodo.SetAttribute("key", "HealthCheck.Extensions.ShowAssemblyFiles")
-        $nuevoNodo.SetAttribute("value", "true")
-        $nodoPadre.PrependChild($nuevoNodo)
-
-        $guardarXml = $true
-        break
+    if ($nodoExistente.GetAttribute("key") -eq "HealthCheck.Extensions.ShowAssemblyFiles") {
+        $nodeNotFoundHealthCheck = $false
     }
 
     #EnableSwagger
-    if ($nodoExistente.Name -eq "add" -and $nodoExistente.GetAttribute("key") -eq "HealthCheck.Extensions.ShowAssemblyFiles") {
-        $nuevoNodo = $xml.CreateElement("add")
-        $nuevoNodo.SetAttribute("key", "EnableSwagger")
-        $nuevoNodo.SetAttribute("value", "true")
-        $nodoPadre.PrependChild($nuevoNodo)
-        $guardarXml = $true
+    if ($nodoExistente.Name -eq "add" -and $nodoExistente.GetAttribute("key") -eq "EnableSwagger") {
+        $nodeNotFoundEnableSwagger = $false
+    }
+
+    if ($nodeNotFoundHealthCheck -eq $false -and $nodeNotFoundEnableSwagger -eq $false) {
         break
     }
+}
+
+if ($nodeNotFoundHealthCheck) {
+    Write-Host "No se encontró ningún nodo <add> con el key 'HealthCheck.Extensions.ShowAssemblyFiles'"
+    # Aquí puedes realizar cualquier acción que necesites cuando no encuentres el nodo
+    $nuevoNodo = $xml.CreateElement("add")
+    $nuevoNodo.SetAttribute("key", "HealthCheck.Extensions.ShowAssemblyFiles")
+    $nuevoNodo.SetAttribute("value", "true")
+    $nodoPadre.PrependChild($nuevoNodo)
+
+    $guardarXml = $true
+}
+
+if ($nodeNotFoundEnableSwagger){
+    Write-Host "No se encontró ningún nodo <add> con el key 'EnableSwagger'"
+    # Aquí puedes realizar cualquier acción que necesites cuando no encuentres el nodo
+    $nuevoNodo = $xml.CreateElement("add")
+    $nuevoNodo.SetAttribute("key", "EnableSwagger")
+    $nuevoNodo.SetAttribute("value", "true")
+    $nodoPadre.PrependChild($nuevoNodo)
+    $guardarXml = $true
 }
 
 if ($guardarXml) {
