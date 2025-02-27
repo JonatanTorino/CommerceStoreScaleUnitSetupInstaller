d10f50c74accc879ac8994558b5fcc199dd6152d
Jonatan Torino <JTorino@axxonconsulting.com>
2025-02-27 12:34:07 +0000

    Add backup file handling in CreateLocalConfigFile.ps1 to restore properties

--- CreateLocalConfigFile.ps1
+++ CreateLocalConfigFile.ps1
@@ -6,13 +6,14 @@ $hostname = $env:COMPUTERNAME
 $configFolder = ".\ConfigFiles"
 $jsonFile = "$configFolder\$hostname.json"
 $fileCount = (Get-ChildItem -Path $configFolder -Filter "$hostname*" -File | Measure-Object).Count
+$jsonBackupFile = ""
 if ($fileCount -gt 0) {
-    Rename-Item $jsonFile -NewName "$hostname.BK$fileCount.json"
+    $jsonBackupFile = "$hostname.BK$fileCount.json"
+    Rename-Item $jsonFile -NewName $jsonBackupFile
 }
 Copy-Item "$configFolder\SAMPLE_Config_By_Env_(DuplicateAndRename).json" $jsonFile
 
 # Cargar archivo
-$jsonFile = "$configFolder\$hostname.json"
 $json = Get-Content $jsonFile | ConvertFrom-Json
 
 # Seteo de configuraciones
@@ -22,6 +23,22 @@ $RetailServerURL = GetWebSiteUrl('RetailServer')
 $json.RetailServerURL = "$RetailServerURL/RetailServer/Commerce"
 $json.CPOSUrl = "$RetailServerURL/POS"
 $json.Thumbprint = GetWebSiteCertThumbprint("AOSService")
+
+# Cargar archivo backup para recuperar algunas propiedades
+if ($fileCount -gt 0) {
+    $jsonBackup = Get-Content $jsonBackupFile | ConvertFrom-Json
+    $json.HWSSetupPath = $jsonBackup.HWSSetupPath
+    $json.HWSChannelConfig = $jsonBackup.HWSChannelConfig
+    $json.CSUSetupPath = $jsonBackup.CSUSetupPath
+    $json.CSUChannelConfig = $jsonBackup.CSUChannelConfig
+    $json.CSUHttpPort = $jsonBackup.CSUHttpPort
+    $json.TelemetryAppName = $jsonBackup.TelemetryAppName
+    $json.AppInsightsInstrumentationKey = $jsonBackup.AppInsightsInstrumentationKey
+    $json.RetailServerAadClientId = $jsonBackup.RetailServerAadClientId
+    $json.CposAadClientId = $jsonBackup.CposAadClientId
+    $json.AsyncClientAadClientId = $jsonBackup.AsyncClientAadClientId
+    $json.IntervalAsyncClient = $jsonBackup.IntervalAsyncClient
+}
 
 # Guardado del archivo
 $json | ConvertTo-Json | Out-File $jsonFile
